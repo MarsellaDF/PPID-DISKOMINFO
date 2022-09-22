908e0da8e90acc8ede963a6079e90f3d8a958dda
Anisa 01 <anisapuji667@gmail.com>
2022-09-22 13:51:45 +0700

    revisi pedoman1

--- PPID_Kominfo/ppid/resources/views/pedoman.blade.php
+++ PPID_Kominfo/ppid/resources/views/pedoman.blade.php
@@ -191,11 +191,11 @@
                     <h1 class="mb-4">DI LINGKUNGAN PEMERINTAH KABUPATEN BANYUWANGI</h1>
                     <h1 class="mb-4">DENGAN RAHMAT TUHAN YANG MAHA ESA</h1>
                     <h1 class="mb-4">BUPATI BANYUWANGI</h1>
-                    <p><i class="far fa-check-circle text-primary me-3"></i>Menimbang:</p>
+                    <p>Menimbang:</p>
                     <p>a. Bahwa dalam rangka mewujudkan penyelenggaraan Pemerintahan yang baik, terbuka, dan akuntabel diperlukan keterbukaan informasi publik sebagai sarana dalam mengoptimalkan partisipasi dan pengawasan publik dalam proses Penyelenggaraan Pemerintahan dan pembangunan, sehingga perlu adanya sistem pelayanan informasi dan dokumentasi.</p>
                     <p>b. Bahwa untuk tersedianya informasi yang dapat dipertanggungjawabkan perlu adanya pedoman untuk mengelola pelayanan informasi di lingkungan Pemerintahan Kabupaten Banyuwangi.</p>
                     <p>c. Bahwa berdasarkan pertimbangan sebagaimana dimaksud huruf a dan huruf b, perlu menetapkan Peraturan Bupati tentang Pedoman Pengelolaan Pelayanan Informasi dan Dokumentasi di Lingkungan Pemerintah Kabupaten Banyuwangi.</p>
-                    <p><i class="far fa-check-circle text-primary me-3"></i>Mengingat:</p>
+                    <p>Mengingat:</p>
                     <p>1. Undang - Undang Nomor 32 Tahun 2004 tentang Pemerintahan Daerah (Lembaran Negara Republik Indonesia Tahun 2004 nomor 125, Tambahan Lembaran Negara Republik Indonesia Nomor 4437) sebagaimana telah diubah kedua kali dengan Undang-Undang Nomor 12 Tahun 2008 (Lembaran Negara Republik Indonesia Tahun 2008 Nomor 59, Tambahan Lembaran Negara Republik Indonesia Nomor 4844);</p>
                     <p>2. Undang - Undang Nomor 33 Tahun 2004 tentang Pertimbangan Keuangan antara Pemerintah Pusat dan Pemerintah Daerah (Lembaran Negara Republik Indonesia Tahun 2004 Nomor 126, Tambahan Lembaran Negara Republik Indonesia Nomor 4438);</p>
                     <p>3. Undang - Undang Nomor 14 Tahun 2008 tentang Keterbukaan Informasi Publik ( Lembaran Negara Republik Indonesia Tahun 2008 Nomor 61, Tambahan Lembaran Negara Republik Indonesia Nomor 4846 );</p>
@@ -210,16 +210,130 @@
                     <p>10. Peraturan Komisi Informasi Nomor 1 Tahun 2010 tentang Standar Layanan Informasi Publik;</p>
                     <p>11. Peraturan Daerah Kabupaten Banyuwangi Nomor 6 Tahun 2011 tentang Organisasi Perangkat Daerah Kabupaten Banyuwangi
                            ( Lembaran Daerah Kabupaten Banyuwangi Tahun 2011 Nomor 1 / D );</p>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">MEMUTUSKAN</h1>
+             </div>
+                    <p>Menetapkan : PERATURAN BUPATI TENTANG PEDOMAN PELAYANAN INFORMASI PUBLIK DI LINGKUNGAN PEMERINTAH KABUPATEN BANYUWANGI.</p>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">BAB 1</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">KETENTUAN UMUM</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">Pasal 1</h1>
+             </div>
+             
+                    <p class="mb-4">Dalam Peraturan Bupati ini yang dimaksud dengan :</p>
+                    <p>1. Daerah adalah Kabupaten Banyuwangi;</p>
+                    <p>2. Pemerintah Daerah adalah Pemerintah Kabupaten Banyuwangi;</p>
+                    <p>3. Bupati adalah Bupati Banyuwangi;</p>
+                    <p>4. Sekretaris Daerah adalah Sekretaris Daerah Kabupaten Banyuwangi;</p>
+                    <p>5. Perangkat Daerah adalah unsur pembantu Bupati dalam penyelenggaraan pemerintahan daerah, yang terdiri dari Sekretariat Daerah, Sekretariat Dewan Pewakilan Rakyat Daerah, Dinas Daerah, Lembaga Teknis Daerah, Kecamatan dan Kelurahan;</p>
+                    <p>6. Informasi adalah keterangan, pernyataan, gagasan dan tanda-tanda yang mengandung nilai, makna dan pesan, baik data, fakta maupun penjelasannya yang dapat dilihat, didengar dan dibaca, yang disajikan dalam berbagai kemasan dan format sesuai dengan perkembangan teknologi informasi dan komunikasi secara elektronik maupun non elektronik; perkembangan teknologi informasi dan komunikasi secara elektronik maupun non elektronik;</p>
+                    <p>7. Dokumentasi adalah pengumpulan, pengolahan, penyusunan dan pencatatan dokumen, data, gambar dan suara untuk bahan informasi publik;</p>
+                    <p>8. Informasi Publik adalah informasi yang dihasilkan, disimpan, dikelola, dikirim dan/atau diterima oleh Pemerintah Daerah yang berkaitan dengan penyelenggaraan Pemerintah Daerah sesuai dengan peraturan perundang-undangan serta informasi lain yang berkaitan dengan kepentingan publik;</p>
+                    <p>9. Badan Publik adalah lembaga eksekutif, legislatif dan badan lain yang fungsi dan tugas pokoknya berkaitan dengan penyelenggaraan negara, yang sebagian atau seluruh dananya bersumber dari Anggaran Pendapatan dan Belanja Negara dan/atau Anggaran Pendapatan dan Belanja Daerah, atau organisasi non pemerintah sepanjang sebagian atau seluruh dananya bersumber dari Anggaran Pendapatan dan Belanja Negara dan/atau Anggaran Pendapatan dan Belanja Daerah, sumbangan masyarakat, dan/atau luar negeri;</p>
+                    <p>10. Pejabat publik adalah orang yang ditunjuk dan diberi tugas untuk menduduki posisi atau jabatan tertentu pada Badan Publik;</p>
+                    <p>11. Pejabat Pengelola Informasi dan Dokumentasi yang selanjutnya disebut PPID adalah pejabat struktural yang bertanggung jawab dalam pengumpulan, pendokumentasian, penyimpanan, pemeliharaan, penyediaan, distribusi dan pelayanan informasi di Pemerintahan Daerah;</p>
+                    <p>12. PPID Pembantu adalah pejabat struktural yang bertanggung jawab dalam pengumpulan, pendokumentasian, penyimpanan, pemeliharaan, penyediaan, distribusi dan pelayanan informasi di lingkungan SKPD;</p>
+                    <p>13. Atasan PPID adalah pejabat yang merupakan atasan langsung PPID;</p>
+                    <p>14. Daftar Informasi Publik adalah catatan yang berisi keterangan secara sistematis tentang seluruh Informasi Publik yang berada dibawah penguasaan Pemerintah Daerah tidak termasuk informasi yang dikecualikan;</p>
+                    <p>15. Pengguna Informasi Publik adalah orang yang menggunakan informasi publik sebagaimana diatur dalam ketentuan peraturan perundang-undangan;</p>
+                    <p>16. Orang adalah orang perseorangan, kelompok orang, badan hukum, atau badan publik;</p>
+                    <p>17. Pemohon Informasi Publik adalah warga negara dan/atau badan hukum Indonesia yang mengajukan permintaan informasi publik sebagaimana diatur dalam ketentuan peraturan perundang-undangan.</p>
 
-                    <p class="mb-4">Dalam proses keterlibatan masyarakat perlu diakomodasikan dengan cara mempermudah jaminan akses informasi publik berdasarkan pedoman pengelolaan informasi dan dokumentasi. Dalam kaitan ini, pengelolaan informasi dan dokumentasi publik diharapkan tidak sampai mengganggu prinsip kehati-hatian dalam menjaga kelangsungan kehidupan berbangsa dan bernegara untuk kepentingan yang lebih luas.</p>
-                    <p class="mb-4">Penerapan prinsip-prinsip good governance ini pada dasarnya sangat tergantung pada persiapan masing-masing Kementerian Komunikasi dan Informatika dalam mengelola informasi dan dokumentasi bagi masyarakat. Untuk itu, sebagai upaya menyamakan persepsi dalam menciptakan dan menjamin kelancaraan dalam pelayanan informasi publik, maka disusun Pedoman Pengelolaan Informasi dan Dokumentasi di lingkungan Kementerian Komunikasi dan Informatika.</p>
+                    <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">BAB II</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">PENGELOLA LAYANAN INFORMASI DAN DOKUMENTASI</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <p>Pasal 2</p>
+             </div>
+                <p class="mb-4">Pemerintahan Daerah yang terdiri dari Pemerintah Daerah dan Dewan Perwakilan Rakyat Daerah sebagai Badan Publik melaksanakan pengelolaan pelayanan informasi dan dokumentasi.</p>
+                    <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <p>Pasal 3</p>
+             </div>
+                    <p>(1) Informasi Publik di lingkungan Pemerintahan Daerah bersifat terbuka dan dapat diakses oleh setiap Pengguna Informasi Publik;</p>
+                    <p>(2) Informasi Publik yang dikecualikan di lingkungan Pemerintahan Daerah bersifat ketat dan terbatas;</p>
+                    <p>(3) Informasi Publik di lingkungan Pemerintahan Daerah dapat diperoleh pemohon informasi publik dengan cepat, tepat waktu, biaya ringan, dan dapat diakses dengan mudah ;</p>
+                    <p>(3) Informasi Publik di lingkungan Pemerintahan Daerah dapat diperoleh pemohon informasi publik dengan cepat, tepat waktu, biaya ringan, dan dapat diakses dengan mudah ;</p>
+                    <p>(5) Informasi Publik yang dikecualikan didasarkan pada pengujian atas konsekuensi yang timbul apabila suatu informasi diberikan kepada masyarakat dan pertimbangan yang seksama bahwa menutup informasi publik dapat melindungi kepentingan yang lebih besar daripada membukanya atau sebaliknya.</p>
+            
+                    <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">BAB III</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">HAK DAN KEWAJIBAN</h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <p>Pasal 4</p>
+             </div>
+                    <P>(1) Pemerintah Daerah berhak menolak memberikan informasi yang dikecualikan;</P>
+                    <P>(2) Informasi yang dikecualikan sebagaimana dimaksud pada ayat (1) ditetapkan lebih lanjut oleh Bupati sesuai dengan kewenangannya;</P>
+                    <P>(3) Pemerintah Daerah berhak menolak memberikan informasi publik apabila tidak sesuai dengan Peraturan Perundang - undangan;</P>
+                    <P>(4) Informasi yang tidak dapat diberikan oleh Pemerintah Daerah, sebagaimana dimaksud pada ayat (1) adalah :</P>
+                    <P>a. Informasi yang dapat membahayakan negara ;</P>
+                    <P>b. Informasi yang dapat berkaitan dengan kepentingan perlindungan usaha dari persaingan usaha tidak sehat ;</P>
+                    <P>c. Informasi yang berkaitan dengan hak - hak pribadi ;</P>
+                    <P>d. Informasi yang berkaitan dengan rahasia jabatan ; dan/atau</P>
+                    <P>e. Informasi yang belum dikuasai atau didokumentasikan.</P>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <p>Pasal 5</p>
+             </div>
+                    <P>(1) Pemerintah Daerah wajib menyediakan, memberikan dan / atau menerbitkan informasi publik yang berada dibawah kewenangannya kepada pemohon informasi publik, selain informasi yang dikecualikan sesuai dengan Peraturan Perundang - undangan;</P>
+                    <P>(2) Untuk melaksanakan kewajiban sebagaimana dimaksud pada ayat (1), Pemerintah Daerah membangun dan mengembangkan sistem informasi dan dokumentasi pengelolaan informasi publik yang dapat diakses dengan mudah.</P>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-4">BAB IV</h1>
+             </div>
+             <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
+                    <h1 class="mb-4"> PEJABAT PENGELOLA INFORMASI DAN DOKUMENTASI </h1>
+             </div>
+             <div class="container-xxl py-1">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <p>Pasal 6</p>
+                    <p>(1) Untuk mengelola pelayanan informasi dan dokumentasi di lingkungan Pemerintahan Daerah ditetapkan PPID;</p>
+                    <p>(2) PPID sebagaimana dimaksud pada ayat (1) melekat pada pejabat struktural yang membidangi tugas dan fungsi pelayanan informasi;</p>
+                    <p>(3) PPID ditetapkan oleh Bupati</p>
+             </div>
                     <p><i class="far fa-check-circle text-primary me-3"></i>Maksud</p>
                     <p>Pedoman pengelolaan Informasi dan Dokumentasi di lingkungan Kementerian Komunikasi dan Informatika dimaksudkan sebagai acuan bagi setiap Satuan Kerja dalam penyediaan, pengumpulan, pendokumentasian dan pelayanan, serta penetapan Pejabat Pengelola Informasi dan Dokumentasi.</p>
                     <p><i class="far fa-check-circle text-primary me-3"></i>Tujuan</p>
                     <p>a. Masing-masing Satuan Kerja mampu menyediakan, mengumpulkan, mendokumentasikan dan menyampaikan informasi tentang kegiatan dan produk unit kerjanya secara akurat dan tidak menyesatkan;</p>
                     <p>b. Satuan Kerja mampu menyediakan, mengumpulkan, mendokumentasikan dan menyampaikan bahan dan produk informasi secara cepat dan tepat waktu;</p>
                     <p>c. Pejabat Pengelola Informasi dan Dokumentasi mampu memberikan pelayanan informasi secara cepat dan tepat waktu dengan biaya ringan dan cara sederhana.</p>
-                </div>
+             </div>
             <!-- </div>
         </div>
     </div> -->
@@ -301,7 +415,7 @@
 
 
     <!-- Feature Start -->
-    <div class="container-fluid bg-primary overflow-hidden my-5 px-lg-0">
+    <!-- <div class="container-fluid bg-primary overflow-hidden my-5 px-lg-0">
         <div class="container feature px-lg-0">
             <div class="row g-0 mx-lg-0">
                 <div class="col-lg-6 feature-text py-5 wow fadeIn" data-wow-delay="0.1s">
@@ -313,8 +427,8 @@
                             <div class="col-6">
                                 <div class="d-flex align-items-center">
                                     <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style="width: 55px; height: 55px;">
-                                        <i class="fa fa-user-md text-primary"></i>
-                                    </div>
+                                        <i class="fa fa-user-md text-primary"></i> -->
+                                    <!-- </div>
                                     <div class="ms-4">
                                         <p class="text-white mb-2">Experience</p>
                                         <h5 class="text-white mb-0">Doctors</h5>
@@ -364,12 +478,12 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Feature End -->
 
 
     <!-- Team Start -->
-    <div class="container-xxl py-5">
+    <!-- <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <p class="d-inline-block border rounded-pill py-1 px-4">Doctors</p>
@@ -435,14 +549,14 @@
                             <div class="team-social text-center">
                                 <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
                                 <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a> -->
+                            <!-- </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Team End -->
 
 
@@ -525,7 +639,7 @@
 
 
     <!-- Testimonial Start -->
-    <div class="container-xxl py-5">
+    <!-- <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <p class="d-inline-block border rounded-pill py-1 px-4">Testimonial</p>
@@ -558,7 +672,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Testimonial End -->
 
 
