e02b4a31605eab55b85c229ba8be475ae016b288
Anisa 01 <anisapuji667@gmail.com>
2022-10-03 14:25:35 +0700

    revisi langsung

--- PPID_Kominfo/ppid/resources/views/langsung.blade.php
+++ PPID_Kominfo/ppid/resources/views/langsung.blade.php
@@ -6,8 +6,8 @@
 
 <div class="container-xxl py-5">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s">
-                    <h1 class="mb-4">Permohonan Informasi Datang Langsung</h1>
-                    <h1 class="mb-4">MEKANISME PERMOHONAN INFORMASI PUBLIK </h1>
+                    <h3 class="mb-4">Permohonan Informasi Datang Langsung</h3>
+                    <h3 class="mb-4">MEKANISME PERMOHONAN INFORMASI PUBLIK </h3>
              </div>
              1. Pemohon informasi datang ke tempat layanan informasi mengisi formulir permintaan informasi dengan melampirkan foto copy KTP pemohon dan pengguna informasi, bagi lembaga dilengkapi foto copy akta pendirian, surat keterangan terdaftar di Kominfo Kabupatem Banyuwangi.<br>
              2. Maksud dan tujuan permintaan informasi harus jelas penggunaannnya.<br>
@@ -16,7 +16,8 @@
              5. Petugas menyerahkan informasi sesuai dengan yang diminta oleh pemohon/pengguna informasi. Jika informasi yang diminta masuk dalam kategori dikecualikan, PPID menyampaikan alasan sesuai dengan keterangan perundangan yang berlaku.<br>
              6. Petugas memberikan Tanda bukti Penyerahan informasi Publik kepada pengguna informasi publik.<br>
              7. Membukukan dan mencatat.
-                    <h1 class="mb-4"> JANGKA WAKTU PENYELESAIAN </h1>
+             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s">
+                    <h3 class="mb-2"> JANGKA WAKTU PENYELESAIAN </h3>
              </div>
              1. Proses penyelesaian untuk memenuhi permintaan pemohon informasi publik dilakukan setelah pemohon informasi memenuhi persyaratan yang telah ditentukan.<br>
              2. Waktu penyelesaian dilaksanakan paling lambat 10 (sepuluh ) hari sejak diterima permintaan Pejabat Pengelola Informasi dan Dokumentasi (PPID) akan menyampaikan pemberitahuan yang berisikan informasi yang diminta berada dibawah penguasaannya atau tidak. Dan PPID dapat memperpanjang waktu paling lambat 14 (empat belas) hari kerja.<br>
