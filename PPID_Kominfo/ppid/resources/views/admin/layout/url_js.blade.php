d5ade51957c464a3b139c45cc08af7bcab9b8d10
Mohammad Haerul Anam <72177592+MarsellaDF@users.noreply.github.com>
2022-10-28 00:07:20 +0700

    Fixing Tampilan Layout Admin

---
+++ PPID_Kominfo/ppid/resources/views/admin/layout/url_js.blade.php
@@ -0,0 +1,6 @@
+<!-- Chart library -->
+<script src="/assets/auth//plugins/chart.min.js"></script>
+<!-- Icons library -->
+<script src="/assets/auth/plugins/feather.min.js"></script>
+<!-- Custom scripts -->
+<script src="/assets/auth/js/script.js"></script>
